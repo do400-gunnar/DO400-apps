61da073718209a7a227c3dad60e1d65c0b4aa0cb
Gunnar Hilling <gunnar@hilling.de>
2026-09-16 13:49:34 +0200

    demo

--- simple-webapp/backend/test.js
+++ simple-webapp/backend/test.js
@@ -1,5 +1,5 @@
 const assert = require("assert").strict;
 const greet = require("./greet");
 
-assert.strictEqual(greet(), "Hello guest");
-assert.strictEqual(greet("Guy"), "Hello Guy");
+assert.strictEqual(greet(), "Hello guest!");
+assert.strictEqual(greet("Guy"), "Hello Guy!");
